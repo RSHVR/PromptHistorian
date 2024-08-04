7cd3681f2cc1c89d1f563c42556da140fc8691aa
Arshveer Gahir <arshveersinghgahir@gmail.com>
2024-08-04 17:06:07 -0400

    Update app.py
    added dependencies

--- app.py
+++ app.py
@@ -4,9 +4,29 @@ import os
 # Webapp Dependencies
 import gradio as gr
 import pandas as pd
-import svelte as sv
+
+# ML Dependencies
+import torch
+import tensorflow as tf
+from transformers import pipeline
 
 # LLM Dependencies
 import cohere as co
 import openai as oa
+import huggingface_hub as hf
+
+#Load API Keys
+openai_key = os.getenv('OPENAI_API_KEY')
+cohere_key = os.getenv('COHERE_API_KEY')
+huggingface_key = os.getenv('HUGGINGFACE_API_KEY')
+gemini_key = os.getenv('GEMINI_API_KEY')
+claude_key = os.getenv('CLAUDE_API_KEY')
+
+# Verify Installation
+## PyTorch
+x = torch.rand(5, 3)
+print(x)
+## Tensorflow
+
+## Transformers
 
